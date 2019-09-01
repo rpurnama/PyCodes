8738d27f5c533ebea251868fb4c6fa10a12095c3
rpoernama <rpurnama@users.noreply.github.com>
2019-09-01 21:56:08 +0700

    Update cmd_del_ios-xrv.py

--- cmd_del_ios-xrv.py
+++ cmd_del_ios-xrv.py
@@ -1,13 +1,22 @@
+#!/user/bin/python2.7
+"""
+Version	: 0.0 (draft-release)
+Author	: Rakhmadian Purnama <rakhmadian.purnama@mastersystem.co.id>
+Source	: https://github.com/rpurnama
+"""
+
 from netmiko import ConnectHandler
 import sys
 
-print("Hi, do you really want to delete the configurations?") 
-print("Please make sure to backup your device configuration!")
+print("Hi, do you really want to delete the configuration?") 
+print("Please make sure to backup your device!")
 print("-----------------------------------------------------\n")
 
 ip_addr = sys.argv[1]
 uid = raw_input("Enter Username: ")
+#uid = sys.argv[2]
 pwd = raw_input("Enter Password: ")
+#pwd = sys.argv[3]
 
 router_xrv = {
 	'device_type': 'cisco_xr',
@@ -22,6 +31,7 @@ connected_device = devconnect.find_prompt()
 print("You are now successfully connected to %s" %connected_device)
 
 cmd_executed = raw_input("Please enter specific command you want to execute: ")
+#cmd_executed = ['list of commands you want to execute in Cisco IOS-XR']
 
 cmd_output = devconnect.send_config_set(cmd_executed)
 print(cmd_output)
